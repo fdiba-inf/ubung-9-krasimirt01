0b27bfdb88ea0d1703bea361cc80b5dfe7d4476b
krasimirt01 <krasimir.tashkov@fdiba.tu-sofia.bg>
2020-11-29 15:24:52 +0000

    -

--- src/exercise9/Ellipse.java
+++ src/exercise9/Ellipse.java
@@ -1,4 +1,63 @@
 package exercise9;
 
 public class Ellipse {
+  private double a;
+  private double b;
+  private Point startPoint;
+
+  public Ellipse() {
+    this.startPoint = new Point(0, 0);
+    a = 1;
+    b = 1;
+  }
+
+  public Ellipse(Point startPoint, double a, double b) {
+    this.startPoint = new Point(startPoint);
+    this.a = a;
+    this.b = b;
+  }
+
+  public Ellipse(Ellipse otherEllipse) {
+    this.startPoint = new Point(otherEllipse.startPoint);
+    this.a = otherEllipse.a;
+    this.b = otherEllipse.b;
+  }
+
+  public boolean isValid() {
+    return a > 0 && b > 0;
+  }
+
+  public void initialise() {
+    System.out.println("Start point: ");
+    startPoint.initialize();
+    System.out.println("Enter a: ");
+    this.a = Utils.INPUT.nextDouble(); 
+    System.out.println("Enter b: ");
+    this.b = Utils.INPUT.nextDouble();
+  }
+
+  public double calculatePerimeter() {
+    return Math.PI * (3 * (a + b) - Math.sqrt(((3 * a) + b) * (a + (3 * b))));
+  }
+
+  public double calculateArea() {
+    return Math.PI * a * b;
+  }
+
+  public String getType() {
+    return (Utils.equals(a, b)) ? "Cycle" : "Ellipse";
+  }
+
+  public String toString() {
+    return String.format("%s-[%s, %s], %s, P=%s, A=%s", startPoint, a, b, getType(), calculatePerimeter(),
+        calculateArea());
+  }
+
+  public boolean equals(Ellipse otherEllipse) {
+    boolean sameA = Utils.equals(this.a, otherEllipse.a);
+    boolean sameB = Utils.equals(this.b, otherEllipse.b);
+    boolean sameAReversed = Utils.equals(this.a, otherEllipse.b);
+    boolean sameBReversed = Utils.equals(this.b, otherEllipse.a);
+    return (sameA && sameB) || (sameAReversed && sameBReversed);
+  }
 }
--- src/exercise9/Utils.java
+++ src/exercise9/Utils.java
@@ -3,11 +3,11 @@ package exercise9;
 import java.util.Scanner;
 
 public class Utils {
-    public static final Scanner INPUT = new Scanner(System.in);
+  public static final Scanner INPUT = new Scanner(System.in);
 
-    private static final double EPSILON = 1e-4;
+  private static final double EPSILON = 1e-4;
 
-    public static boolean equals(double x, double y) {
-        return Math.abs(x - y) < EPSILON;
-    }
+  public static boolean equals(double x, double y) {
+    return Math.abs(x - y) < EPSILON;
+  }
 }
